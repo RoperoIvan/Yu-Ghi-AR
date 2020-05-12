a1c6b05d05fa7366924e1ae7ffe2cf27d775ea9e
didaclis <36189205+didaclis@users.noreply.github.com>
2020-05-12 09:52:48 +0200

    2 dragons

---
+++ Yu-Ghi-AR/Assets/Script/PointCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCounter : MonoBehaviour
+{
+
+    public int wins = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wins = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wins == 2)
+            Debug.Log("win");
+            //win condition
+    }
+
+    public void WinRound()
+    {
+        ++wins;
+    }
+}
