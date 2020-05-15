e718a5bebd1f0e4f89e246389213c30eac3b83e7
Enric <36191828+PerezEnric@users.noreply.github.com>
2020-05-15 12:46:51 +0200

    MainMenu done

---
+++ Yu-Ghi-AR/Assets/UI/SetVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetMasterVolume(float slidervalue)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(slidervalue)*20);
+    }
+    public void SetMusicVolume(float slidervalue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(slidervalue) * 20);
+    }
+    public void SetSFXVolume(float slidervalue)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(slidervalue) * 20);
+    }
+}
