22879908e58ba754a537d753a1adfd5e9f676318
RoperoIvan <ivan_ropero98@hotmail.com>
2020-05-19 12:03:32 +0200

    minigame

---
+++ Yu-Ghi-AR/Assets/Script/PressMiniGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressMiniGame : MonoBehaviour
+{
+    public int points = 0;
+    public GameObject healthBar;
+    SimpleHealthBar gmae;
+    float value = 100;
+    float timer = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timer = Time.realtimeSinceStartup;
+        gmae = healthBar.GetComponent<SimpleHealthBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(value < 100)
+        {
+            value += 0.01f;
+            gmae.UpdateBar(value, 100);
+        }
+
+    }
+    public void PressPoints()
+    {
+        if(value > 0)
+            gmae.UpdateBar((value -=2),100);
+    }
+}
